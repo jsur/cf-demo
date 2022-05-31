783d7ff2a57acc2c58a0f8160c6b4c9d5cd86da5
julius.suominen <julius.suominen@gmail.com>
2022-05-31 10:00:10 +0300

    use NodeJsFunction

--- cdk/lib/lambda-stack.ts
+++ cdk/lib/lambda-stack.ts
@@ -1,15 +1,17 @@
 import { Stack, StackProps } from "aws-cdk-lib";
-import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Function(this, "demo-lambda", {
+    // NodejsFunction uses esbuild to automatically transpile ts into js!
+    new NodejsFunction(this, "demo-lambda", {
       runtime: Runtime.NODEJS_14_X,
-      handler: "handler.demo",
-      code: Code.fromAsset("lib/lambda"),
+      handler: "demo",
+      entry: "./lib/lambda/handler.ts",
     });
   }
 }
